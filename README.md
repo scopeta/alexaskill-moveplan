f9da4b80fe0a28d60eb78bbc7f1230ba7907b8bd
Fabio Scopeta <fscopeta@microsoft.com>
2019-10-07 18:20:39 -0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# alexaskill-moveplan
+Alexa skill that helps you organize weekly workouts.
+
+Flask app running on Azure Web App for Linux
+
+Terraform script creates basic infrastructure on Azure
--- move-plan-app.tf
+++ move-plan-app.tf
@@ -9,6 +9,8 @@ resource "azurerm_app_service_plan" "svsplan" {
   name                = "alexa-appserviceplan"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
+  kind                = "Linux"
+  reserved            = true
 
   sku {
     tier = "Basic"
@@ -22,8 +24,12 @@ resource "azurerm_app_service" "app" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   app_service_plan_id = "${azurerm_app_service_plan.svsplan.id}"
   site_config {
-    always_on      = true
-    python_version = "3.4"
+    always_on        = true
+    app_command_line = "gunicorn --bind=0.0.0.0 --timeout 600 app:app"
+    linux_fx_version = "PYTHON|3.6"
+    scm_type         = "GitHub"
   }
+
+
 
 }
